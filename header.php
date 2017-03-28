d411d4cab199e9cca1c61c13f6fbef38ab5aca05
Heather Corey <heather@bowst.com>
2017-03-28 16:39:14 -0400

    add favicon code

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <?php wp_head(); ?>
 </head>
 
