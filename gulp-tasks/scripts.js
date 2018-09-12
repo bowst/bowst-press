4b0bb1fe14b8fd90b6ef3e61f65ae736c6f0afab
Johnny Hoell <johnny.hoell@gmail.com>
2018-09-12 17:33:12 -0400

    updates FontAwesome to 5.3.1

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -31,19 +31,7 @@ let webpackConfig = {
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery', // Used for Bootstrap JavaScript components
-            jQuery: 'jquery', // Used for Bootstrap JavaScript components
-            Popper: ['popper.js', 'default'], // Used for Bootstrap dropdown, popup and tooltip JavaScript components
-            Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
-            Button: 'exports-loader?Button!bootstrap/js/dist/button',
-            Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
-            Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
-            Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
-            Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
-            Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
-            Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
-            Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
-            Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
-            Util: 'exports-loader?Util!bootstrap/js/dist/util'
+            jQuery: 'jquery' // Used for Bootstrap JavaScript components
         }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
