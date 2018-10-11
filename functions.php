4000d58d4ad2ecb31679e080f27f5ffdc943773c
Johnny Hoell <johnny.hoell@gmail.com>
2018-10-11 15:28:31 -0400

    Localize site URLs for use in JavaScript

--- functions.php
+++ functions.php
@@ -127,6 +127,18 @@ function bowst_press_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	/**
+	 * Localize site URLs for use in JavaScripts
+	 * Usage: SiteInfo.theme_directory + '/scripts/widget.js'
+	 */
+	$site_info = array(
+		'homeUrl'        => get_home_url(),
+		'themeDirectory' => get_template_directory_uri(),
+		'post_type'      => get_post_type(),
+		'ajaxurl'        => admin_url( 'admin-ajax.php' ),
+	);
+	wp_localize_script( 'bowst-press-global-scripts', 'SiteInfo', $site_info );
 }
 add_action( 'wp_enqueue_scripts', 'bowst_press_scripts' );
 
